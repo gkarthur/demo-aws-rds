264a0980c40bdccaf76a34f75c1ad0d181f022be
Administrator <admin@example.com>
2018-10-15 11:10:20 +0200

    update demo rs

--- src/main/java/com/example/aws/demoawsrds/DemoAwsRdsApplication.java
+++ src/main/java/com/example/aws/demoawsrds/DemoAwsRdsApplication.java
@@ -2,9 +2,26 @@ package com.example.aws.demoawsrds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class DemoAwsRdsApplication {
+public class DemoAwsRdsApplication extends SpringBootServletInitializer {
+	
+	/**
+	 * configure method do configure in EE server (war mode)
+	 */
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoAwsRdsApplication.class);
+    }
+	
+	/**
+	 * Jar mode
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
         new SpringApplication(DemoAwsRdsApplication.class).run(args);
     }
